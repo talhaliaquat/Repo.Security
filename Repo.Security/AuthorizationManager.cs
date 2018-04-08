399590c2b106be106736668c175ab95ee836d24d
Talha Liaquat <talha.liaquat@gmail.com>
2018-04-09 02:27:34 +0400

    Init
    Create Source Code

---
+++ Repo.Security/AuthorizationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.Security
+{
+    public class AuthorizationManager : ServiceAuthorizationManager
+    {
+        public override bool CheckAccess(OperationContext operationContext)
+        {
+            IncomingWebRequestContext request = WebOperationContext.Current.IncomingRequest;
+            WebHeaderCollection headers = request.Headers;
+            var customHeader = headers["Authentication"];
+            return customHeader != null && !string.IsNullOrEmpty(customHeader);
+        }
+    }
+}
